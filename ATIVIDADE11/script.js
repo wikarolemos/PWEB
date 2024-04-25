6e9c99967c6045c1a970aa87977471351fb7795f
Wikaro Lemos <107596984+wikarolemos@users.noreply.github.com>
2024-04-24 22:00:57 -0300

    Add files via upload

---
+++ ATIVIDADE11/script.js
@@ -0,0 +1,18 @@
+var funcionario1 = new Object()
+funcionario1.matricula = "998422"
+funcionario1.nome = "Wikaro"
+funcionario1.funcao = "Cientista de Dados"
+alert(`MATRÍCULA: ${funcionario1.matricula} | NOME: ${funcionario1.nome} | FUNÇÃO: ${funcionario1.funcao}`)
+
+var funcionario2 = {}
+funcionario2.matricula = "99823"
+funcionario2.nome = "Jorge"
+funcionario2.funcao = "Gerente TI"
+alert(`MATRÍCULA: ${funcionario2.matricula} | NOME: ${funcionario2.nome} | FUNÇÃO: ${funcionario2.funcao}`)
+
+var funcionario3 = {
+    matricula: "99356",
+    nome: "Carlos",
+    funcao: "Gerente RH"
+}
+alert(`MATRÍCULA: ${funcionario3.matricula} | NOME: ${funcionario3.nome} | FUNÇÃO: ${funcionario3.funcao}`)
